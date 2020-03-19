21bfad0b6354730032204b00f234ab623a7a8f76
jackson-elfers <jacksonelfersdev@outlook.com>
2020-03-18 19:46:21 -0700

    init: first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Jackson Elfers</p>
+      <h1>Hey I'm <span style = {{ color: "white" }}>Jackson</span></h1>
+      <hr/>
+      <h2>
+      I build applications with <a target = "_blank" href = "https://reactjs.org/">React</a> and cloud based services with <a target = "_blank" href = "https://aws.amazon.com/">AWS</a>.
+      </h2>
+      <h2>
+      I'm part of <a target = "_blank" href = "https://github.com/jackson-elfers">GitHub</a>, the world's largest open source community.
+      </h2>
+      <h2>
+      I'm always looking for where I can help, I'd love to chat on <a target = "_blank" href = "https://www.linkedin.com/in/jackson-elfers/">LinkedIn</a>.
+      </h2>
+      
+      <p>Client Work</p>
+      <a target = "_blank" href = "https://www.giocars.com">giocars.com</a>
+      <p>GitHub Contributions</p>
+      <a target = "_blank" href = "https://github.com/taniarascia/takenote">github.com/taniarascia/takenote</a>
+      <p>Independent Projects</p>
+      <a target = "_blank" href = "https://github.com/jackson-elfers/bontaki">github.com/jackson-elfers/bontaki</a>
+      <p>Contact Info</p>
+      <p style = {{ color: "white" }}>jacksonelfersjobs@outlook.com</p>
     </div>
   );
 }
