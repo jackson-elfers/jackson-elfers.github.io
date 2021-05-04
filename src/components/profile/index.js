026a340794d82f0baaf20c6270d68abbf3162cb1
jackson-elfers <jacksonelfersdev@outlook.com>
2021-05-04 15:43:12 -0700

    add: typing

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,23 +1,64 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Typewriter from 'typewriter-effect/dist/core';
 import css from "./index.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
 function Main(props) {
+  const [index, setIndex] = useState(0);
+
+  function titleEffect() {
+    const typewriter = new Typewriter(document.getElementById("panelTitle"), {
+      loop: false,
+      delay: 75,
+    });
+    typewriter
+    .typeString(props.panelComponent[index].title)
+    .pauseFor(1000)
+    .start();
+  }
+
+  function bodyEffect() {
+    const typewriter = new Typewriter(document.getElementById("panelBody"), {
+      loop: false,
+      delay: 75,
+    });
+    typewriter
+    .typeString(props.panelComponent[index].body)
+    .pauseFor(1000)
+    .start();
+  }
+
+  function typing() {
+    titleEffect();
+    bodyEffect();
+  }
+
+  useEffect(() => {
+    typing();
+  }, []);
+
+  useEffect(() => {
+    typing();
+  }, [index]);
+
   return (
     <div>
       <div className="box peddapViewer">
         <Carousel
+          onChange={function(i) { setIndex(i); }}
           renderThumbs={() => {
             return [];
           }}
         >
-          {props.viewerComponent}
-          <img src="https://g.foolcdn.com/editorial/images/616486/square01.jpg" />
+          {props.panelComponent.map((d) => { return <img src = {d.media}/> })}
         </Carousel>
       </div>
 
-      <div className="box peddapPanel">{props.panelComponent}</div>
+      <div className="box peddapPanel">
+        <h2 id = "panelTitle"></h2>
+        <p id = "panelBody"></p>
+      </div>
     </div>
   );
 }
