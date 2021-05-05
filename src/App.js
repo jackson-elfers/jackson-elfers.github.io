9875b8bb3659a0a59bfd4019b754feeb781cd69a
jackson-elfers <jacksonelfersdev@outlook.com>
2021-05-04 20:24:33 -0700

    add: things

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Profile } from "./components";
 import './styles';
+import directives from "./logs/directives";
 
 function App() {
 
   return (
     <div>
-    <h1>Jackson T. Elfers</h1>
+    <h3>Jackson Thomas Elfers</h3>
     <div style={{ float: "left", width: "100%" }}>
       <div className="box">
-        <h2>Goals, Plans and Visions for the Future.</h2>
-        <p>A collection of ideas for those who find my behavior curious and for my personal record keeping.</p>
+        <h2>They used to call me "Doubting Thomas" at catholic school.</h2>
+        <p>When she asks me about my future I say I prefer them over stocks and bonds.</p>
       </div>
       <Profile
-        panelComponent={[{ media: "./heart.png", title: "titleOne", body: "bodyOne" }, { media: "https://g.foolcdn.com/editorial/images/616486/square01.jpg", title: "titleTwo", body: "bodyTwo" }]}
+        panelComponent={directives}
       />
     </div>
     
---
+++ src/logs/directives/index.js
@@ -0,0 +1,8 @@
+export default [
+	{ "media": "https://g.foolcdn.com/editorial/images/616486/square01.jpg", "title": "The universe is my bae.", "body": "Not long ago in a lucid dream, I was embraced by the universe. We spooned, obviously since I'm much smaller I took little spoon (we're just really good friends) but that's beside the point..." },
+	{ "media": "https://g.foolcdn.com/editorial/images/616486/square01.jpg", "title": "Seriously we're just friends.", "body": "The universe spoke three fundamental directives..." },
+	{ "media": "https://g.foolcdn.com/editorial/images/616486/square01.jpg", "title": "I'm not religious or whatever.", "body": "I. Liberate every woman and man from external governing entities by decentralizing energy, food and housing using automation and technological process improvements...." },
+	{ "media": "https://g.foolcdn.com/editorial/images/616486/square01.jpg", "title": "I'm not a psychologist.", "body": "II. Improve the processes in which we perceive our world with innovations in psychological techniques..." },
+	{ "media": "https://g.foolcdn.com/editorial/images/616486/square01.jpg", "title": "Evolution baby!", "body": "III. Design a world that is aligned with the individualistic biological reward system of woman and man..." },
+	{ "media": "https://g.foolcdn.com/editorial/images/616486/square01.jpg", "title": "What the hell does it mean?", "body": "I woke up with a hangover and was literally covered with hair of the dog since we sleep on the same couch. As for dream interpretation your guess is as good as mine." }
+]
