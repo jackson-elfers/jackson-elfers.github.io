1b982039d7466e0b5998eae6ae22dc2b1f9e6823
jackson-elfers <jacksonelfersdev@outlook.com>
2021-05-06 04:08:02 -0700

    add: husky

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Profile, Fish } from "./components";
-import Typewriter from 'typewriter-effect';
-import './styles';
+import Typewriter from "typewriter-effect";
+import "./styles";
 import directives from "./logs/directives";
 
 function App() {
-
   return (
     <div>
-    <h1 style = {{ margin: "0%" }}>Jackson</h1>
-    <h1 style = {{ margin: "0%" }}><Typewriter
-  options={{
-    strings: ['Thomas', 'Doubting', "Introvert", "Psychopath"],
-    autoStart: true,
-    loop: true,
-  }}
-/></h1>
-    <h1 style = {{ margin: "0%" }}>Elfers</h1>
-    <div style={{ float: "left", width: "100%" }}>
-      <div className="box">
-        <h2>The kids used to call me "Doubting Thomas" at Catholic school.</h2>
-        <p>When she asks me about my future I say I prefer them over stocks and bonds.</p>
+      <h1 style={{ margin: "0%" }}>Jackson</h1>
+      <h1 style={{ margin: "0%" }}>
+        <Typewriter
+          options={{
+            strings: ["Thomas", "Doubting", "Introvert", "Psychopath"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </h1>
+      <h1 style={{ margin: "0%" }}>Elfers</h1>
+      <div style={{ float: "left", width: "100%" }}>
+        <div className="box">
+          <h2>The kids used to call me "Doubting Thomas" at Catholic school.</h2>
+          <p>When she asks me about my future I say I prefer them over stocks and bonds.</p>
+        </div>
+        <Profile panelComponent={directives} />
       </div>
-      <Profile
-        panelComponent={directives}
-      />
-    </div>
-
     </div>
   );
 }
